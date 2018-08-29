d20fdf4e36553268dc979ca3ac1973a7d2fe7b71
Peder Tinholt Kristensen <39559066+kredep@users.noreply.github.com>
2018-08-29 09:39:46 +0200

    Create script.js

---
+++ 35/fagdag/variabler2/scripts/script.js
@@ -0,0 +1,12 @@
+window.onload = startUp;
+
+function startUp() {
+    /**
+     * Start-funksjon.
+     */
+    var tall1 = 14;
+    var tall2 = 8;
+    console.log("Sum: " + (tall1+tall2));
+    console.log("Produkt: " + (tall1*tall2));
+    console.log("Differanse: " + (tall1-tall2));
+}
