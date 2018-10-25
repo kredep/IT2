3f25583e865d984533e5e152ed627d5e95cf24b3
Peder Tinholt Kristensen <39559066+kredep@users.noreply.github.com>
2018-10-25 12:27:23 +0200

    kommentarer

--- 43/arrayoppgaver/scripts/script.js
+++ 43/arrayoppgaver/scripts/script.js
@@ -1,9 +1,13 @@
+// Kjører startfunksjonen etter siden har lastet inn
 window.onload = startUp;
 
+// Deklarerer en array for bokstavene
+// Bruker [] og ikke 'new Array()' da dette er dårlig praksis
 var myArray = [];
 
 function startUp() {
     skrivUt();
+    // Lytter til knapper
     document.getElementById("legg-til").onclick = nyBokstav;
     document.getElementById("sort").onclick = sort;
     document.getElementById("annenhver").onclick = annenHver;
@@ -11,17 +15,26 @@ function startUp() {
 }
 
 function sort() {
+    // Sorterer lista og oppdaterer på nettsiden ved å skrive ut på nytt
     myArray.sort();
     skrivUt();
 }
 
 function nyBokstav() {
+    /**
+     * Legger til en ny bokstav i lista
+     * Oppdaterer nettsiden ved å skrive ut på nytt
+     */
     var bokstav = document.getElementById("bokstav").value;
     myArray.push(bokstav);
     skrivUt();
 }
 
 function skrivUt() {
+    /**
+     * Skriver ut arrayen ved å traversere arrayen
+     * Sender feilmelding dersom arrayen er tom
+     */
     if (myArray.length > 0) {
         document.getElementById("liste").innerHTML = "<ul>";
         for (let i=0;i<myArray.length;i++) {
@@ -34,6 +47,10 @@ function skrivUt() {
 }
 
 function annenHver() {
+    /**
+     * Funksjon som skriver ut annen hver av elementene i arrayen
+     * Sender feilmelding dersom lista er tom
+     */
     if (myArray.length > 0) {
         document.getElementById("annenhver-div").innerHTML = "<ul>";
         for (let i=0;i<myArray.length;i+=2) {
@@ -46,11 +63,14 @@ function annenHver() {
 }
 
 function oppgave2() {
+    /**
+     * Funksjon som løser oppgave 2
+     * Traverserer lista og finner statistikken som trengs
+     */
     var tall = [34,53,2,3,34,26,26,85,3,4,98,2,12];
 
     var sum = 0;
     var negative = 0;
-    var snitt = 0;
     var lavest = 0;
     var sumPartall = 0;
 
@@ -68,7 +88,9 @@ function oppgave2() {
             sumPartall += tall[i];
         }
     }
-    snitt = (sum / tall.length).toFixed(2);
+    var snitt = (sum / tall.length).toFixed(2);
+    
+    // Skriver ut resultatene til nettsiden
     document.getElementById("resultat").innerHTML = 
         "<br>Sum: " + sum + 
         "<br>Negative tall: " + negative + 
