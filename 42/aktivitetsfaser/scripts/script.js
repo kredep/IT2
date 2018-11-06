9c0f2251841ac44e2cf24b39c3239a40d1dec21c
Peder Tinholt Kristensen <39559066+kredep@users.noreply.github.com>
2018-11-06 18:22:33 +0100

    rework

--- 42/aktivitetsfaser/scripts/script.js
+++ 42/aktivitetsfaser/scripts/script.js
@@ -1,50 +1,36 @@
 window.onload = startUp;
 
-var counter = -1;
-var i = 0;
-var prev = -1;
-
-// Tidsbruk
-var tidsbruk = [4,3,2,3];
-
-// Navn på bildefiler
-var bilder = ["sleep.jpg", "school.jpg", "freetime.jpg", "rest.jpg"];
-
-// Kaloribruk (kcal)
-var calories = [90, 110, 130, 110];
+var time = 0;
+var interval;
 
 function startUp() {
-    TweenMax.to(image,0.25,{opacity: 0, delay: 4 - 0.25});
-    faser();
-    setInterval(faser, 1000);
+    timer();
+    var interval = setInterval(timer, 1000);
 }
 
-function faser() {
-    counter += 1;
-    if (counter >= 0 && counter < 4) {
-        // Sleep
-        prev = i;
-        i = 0;
-    } else if (counter >= 4 && counter < 7) {
-        // School
-        prev = i;
-        i = 1;
-    } else if (counter >= 7 && counter < 9) {
-        // Freetime
-        prev = i;
-        i = 2;
-    } else if (counter >= 9 && counter < 12) {
-        // Rest
-        prev = i;
-        i = 3;
-    } else if (counter >= 12) {
-        counter = 0;
-        i = 0;
+function timer() {
+    var image = document.getElementById("image");
+    var usage = document.getElementById("usage");
+    var nrg = document.getElementById("nrg");
+
+    if (time == 0) {
+        image.style.backgroundImage = 'url(images/sleep.jpg)';
+        usage.innerHTML = "Energiforbruk: 90 kcal/time";
+        nrg.style.width = (90/120) * 700 + "px";
+    } else if (time == 4) {
+        image.style.backgroundImage = 'url(images/school.jpg)';
+        usage.innerHTML = "Energiforbruk: 110 kcal/time";
+        nrg.style.width = (110/120) * 700 + "px";
+    } else if (time == 7) {
+        image.style.backgroundImage = 'url(images/freetime.jpg)';
+        usage.innerHTML = "Energiforbruk: 120 kcal/time";
+        nrg.style.width = 700 + "px";
+    } else if (time == 9) {
+        image.style.backgroundImage = 'url(images/rest.jpg)';
+        usage.innerHTML = "Energiforbruk: 110 kcal/time";
+        nrg.style.width = (110/120) * 700 + "px";
+    } else if (time == 12) {
+        time = 0;
     }
-    if (prev != i) {
-        TweenMax.set(image,{attr:{src: "images/" + bilder[i]}});
-        document.getElementById("nrg").innerHTML = "Energiforbruk: " + calories[i] + " kcal / time";
-        TweenMax.to(image,0.25,{opacity: 1});
-        TweenMax.to(image,0.25,{opacity: 0, delay: tidsbruk[i] - 0.25});
-    }
+    time++;
 }
