6d7d1ac93ee7097f2e06b0ca23945930dc588927
Peder Tinholt Kristensen <39559066+kredep@users.noreply.github.com>
2018-11-14 22:31:30 +0100

    Update wallpapers.js

--- frontpage/wallpapers.js
+++ frontpage/wallpapers.js
@@ -10,7 +10,7 @@ var urls = [
 
 function startUp() {
     wallpaperChange();
-    setInterval(wallpaperChange, intervaltime);
+    setInterval(wallpaperChange, intervalTime);
 }
 
 function wallpaperChange() {
