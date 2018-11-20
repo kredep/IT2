5c63840bb1872c2cda57501b5678cac822fdceb4
Peder Tinholt Kristensen <39559066+kredep@users.noreply.github.com>
2018-11-20 09:57:38 +0100

    Update script.js

--- 47/teoriprove/scripts/script.js
+++ 47/teoriprove/scripts/script.js
@@ -1,53 +1,79 @@
 window.onload = startUp;
 
-var score = 0;
-var currentQuestion = -1;
-var answers = [];
+var score = 0;              // scoren til spilleren
+var currentQuestion = -1;    // indexen til nåværende spørsmål
+var answers = [];           // array for å holde styr på riktige og gale svar. 0 = feil, 1 = riktig
+/**
+ * question - tekstspørsmålet
+ * multimedia - ferdig html-tag med kilde
+ * alternatives - liste med alternativer til svar
+ * correct - index til riktig alternativ i alternatives-arrayen
+ */
 var questions = [
     {
-        question: "Hvem er dette?",
-        multimedia: '<img src="https://yt3.ggpht.com/a-/AJLlDp3NBEAncMbfNgUps0z0KS_oTP7bYh9hZB3i-Q=s900-mo-c-c0xffffffff-rj-k-no" width="400">',
+        question: "Hvor mange trekanter er det her?",
+        multimedia: '<img src="https://i.imgur.com/Sy9l3Pp.jpg" width="300">',
         alternatives: [
-            "Petter",
-            "Jesper",
-            "Bissan",
-            "Shaggy"
-        ],
-        correct: "Shaggy"
-    },
-    {
-        question: "Hvor mange farger er det her?",
-        multimedia: '<img src="https://data.whicdn.com/images/265031486/original.gif">',
-        alternatives: [
-            "Alle sammen",
-            "10",
             "24",
-            "12"
+            "12",
+            "18",
+            "30"
         ],
-        correct: "10"
+        correct: 2
     },
     {
-        question: "Hva heter denne emoten?",
-        multimedia: '<img src="https://cdn.betterttv.net/emote/5baa5b59f17b9f6ab0f3e84f/2x">',
+        question: "Hvilket land sitt flagg er dette?",
+        multimedia: '<img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1280px-Flag_of_the_United_Kingdom.svg.png" width="300">',
         alternatives: [
-            "Pepe",
-            "pepeJAMMER",
-            "PepeHands",
-            "monkaS"
+            "Norge",
+            "England",
+            "Australia",
+            "Storbritannia"
         ],
-        correct: "pepeJAMMER"
+        correct: 3
+    },
+    {
+        question: "Hvem er dette?",
+        multimedia: '<img src="http://onarki.no/blogg/wp-content/uploads/2016/05/HaraldEia.jpeg">',
+        alternatives: [
+            "Harald Eia",
+            "Bård Tufte Johansen",
+            "Jon Almaas",
+            "Espen Eckbo"
+        ],
+        correct: 0
+    },
+    {
+        question: "Hvor mange meter over havet ligger toppen av Mount Everest?",
+        multimedia: '<img src="https://cdn.britannica.com/s:500x350/17/83817-004-C5DB59F8.jpg">',
+        alternatives: [
+            "8848 meter",
+            "8484 meter",
+            "8448 meter",
+            "8842 meter"
+        ],
+        correct: 0
     }
 ];
 
 function startUp() {
+    /**
+     * Startfunksjon
+     * Laster spørsmål og lager lytter til knappen
+     */
     nextQuestion();
     document.getElementById("nextQ").onclick = checkQuestion;
 }
 
 function checkQuestion() {
+    /**
+     * Sjekker svaret på oppgaven
+     * Laster neste spørsmål etter å ha sjekket svar
+     */
     if (document.querySelector('input[name="answer"]:checked') != null) {
         var answer = document.querySelector('input[name="answer"]:checked').value;
-        var correct = questions[currentQuestion]["correct"];
+        var index = questions[currentQuestion]["correct"];
+        var correct = questions[currentQuestion]["alternatives"][index];
         if (answer == correct) {
             score += 1;
             answers.push(1);
@@ -61,6 +87,9 @@ function checkQuestion() {
 }
 
 function nextQuestion() {
+    /**
+     * Laster neste spørsmål
+     */
     currentQuestion++;
     if (currentQuestion == questions.length) {
         // Vis resultater
@@ -68,9 +97,9 @@ function nextQuestion() {
         var prcnt = ((score/questions.length)*100).toFixed(2);
         var bdy = document.getElementById("bdy");
         bdy.innerHTML = "<h1>Resultat</h1>";
-        bdy.innerHTML += '<h2>Du fikk ' + score + ' / ' + questions.length + ' spørsmål riktig. Med en svarprosent på ' + prcnt + '%</h2>';
+        bdy.innerHTML += '<h2>Du fikk ' + score + ' / ' + questions.length + ' spørsmål riktig, som tilsvarer ' + prcnt + '% riktige svar.</h2>';
         if (score == questions.length) {
-            bdy.innerHTML += "<h2>Du svarte dermed riktig på alle spørsmålene!</h2>";
+            bdy.innerHTML += "<h2>Gratulerer!</h2>";
             bdy.innerHTML += '<img src="https://cdn.betterttv.net/emote/5590b223b344e2c42a9e28e3/2x"><img src="https://cdn.betterttv.net/emote/55b6f480e66682f576dd94f5/2x">'
         } else {
             bdy.innerHTML += "<h4>Du svarte feil på:</h4><ul>"
@@ -87,10 +116,25 @@ function nextQuestion() {
         var aCon = document.getElementById("answer");
         qCon.innerHTML = questions[currentQuestion]["question"];
         mCon.innerHTML = questions[currentQuestion]["multimedia"];
-        var alts = questions[currentQuestion]["alternatives"];
+        var alts = questions[currentQuestion]["alternatives"].slice(0,questions[currentQuestion]["alternatives"].length);
+        alts = shuffle(alts);
         aCon.innerHTML = "";
         for (let i=0;i<alts.length;i++) {
             aCon.innerHTML += '<input type="radio" class="check-container" name="answer" value="' + alts[i] + '"> ' + alts[i] + '<br>';
         }
     }
+}
+
+function shuffle(a) {
+    /**
+     * Funksjon som gir elementene i en array en tilfeldig plassering
+     */
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
 }
